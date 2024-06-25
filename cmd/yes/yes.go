2e610170675f5f9fcd436ee21ffebb4e2bed10b4
abdahmed22 <abd.ahmed03@eng-st.cu.edu.eg>
2024-06-25 14:52:36 +0300

    yes done

---
+++ cmd/yes/yes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+    "fmt"
+)
+
+func main() {
+
+	args := os.Args[1:]
+
+	if len(args) != 0 {
+		for {
+			for i:= range len(args) {
+				fmt.Printf("%s ",args[i])
+			}
+			fmt.Printf("\n")
+		}
+	} else {
+		for {
+			fmt.Printf("y\n")
+		}
+	}
+}
