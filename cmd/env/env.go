044a7e074b5317f09021d0ba3afa162bb1d6bcd2
abdahmed22 <abd.ahmed03@eng-st.cu.edu.eg>
2024-06-25 14:41:02 +0300

    tree done

---
+++ cmd/env/env.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+    "fmt"
+)
+
+func main() {
+	var newLineFlag bool
+
+    flag.BoolVar(&newLineFlag, "n", false, "New Line")
+
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) != 0 {
+		for i:= range len(args) {
+			fmt.Printf("%s ",args[i])
+		}
+	}
+
+	if !newLineFlag {
+		fmt.Printf("\n")
+	}
+
+
+
+
+}
